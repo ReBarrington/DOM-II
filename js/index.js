309daf1ef41c74401c591d5520a22a27e372091f
Reagan Barrington <reagan@barringtonfamily.net>
2020-02-11 15:42:28 -0500

    4 events in

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+
+// H1
+const h1 = document.querySelector('h1');
+// mouse enter
+h1.addEventListener('mouseenter', () => {
+    h1.style.transform = 'scale(1.5)';
+    h1.style.transition = 'transform 0.3s';
+})// mouse leave
+h1.addEventListener('mouseleave', () => {
+    h1.style.transform = 'scale(1)';
+})
+
+// Nav Items
+// turns orange when clicked once
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        link.style.color = 'orange';
+    })
+})
+// turns back to black when mouse over
+navLinks.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        link.style.color = 'black';
+    })
+})
